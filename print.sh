70e6b689f75b82ad5577ec49747eea8206cfea05
Kevin Garubba <kevingarubba@gmail.com>
2020-11-21 11:37:58 -0500

    Adding test frontend directory, 'test' so that I can run tests on whether this works on both docker and podman.

--- print.sh
+++ print.sh
@@ -1,6 +1,6 @@
-if [ -f .env ]
+if [ -f ./config/.env ]
 then
-  export $(cat .env | sed 's/(\|)//g' | xargs)
+  export $(cat ./config/.env | sed 's/(\|)//g' | xargs)
 fi
 # echo "$WORDPRESS_ADMIN_USER"
 echo $WORDPRESS_DESCRIPTION
