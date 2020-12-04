701e2f666b2f7681c56b0d1e31e72bcb60feac64
Kevin Garubba <kevingarubba@gmail.com>
2020-12-03 22:21:55 -0500

    Maybe forgot to commit file changes in docker-compose.

--- test/wordpress/wp-content/themes/twentytwenty/header.php
+++ test/wordpress/wp-content/themes/twentytwenty/header.php
@@ -31,7 +31,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group" role="banner">
-asdfasdf
+Things and stuff
 			<div class="header-inner section-inner">
 
 				<div class="header-titles-wrapper">
