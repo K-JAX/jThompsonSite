7c71387f920a566e938c4cd198cbc16396db3f0f
Kevin Garubba <kevingarubba@gmail.com>
2020-12-04 12:01:23 -0500

    Fixing permission issue in frontend Dockerfile. Making the kill  script conditional on podman/docker.

--- kill.sh
+++ kill.sh
@@ -1,6 +1,16 @@
-# podman pod stop icm && \
-# podman pod rm icm && \
-podman stop --all && \
-podman pod stop --all && \
-yes | podman system prune && \
-yes | podman volume prune
+#!/bin/bash
+
+# Redhat/Fedora/CentOS
+if [ -f /etc/redat-release ]
+then
+    SOFTWARE='podman'
+elif [[ -f /etc/debian_version || "$OSTYPE" == "darwin"* ]]
+then
+    SOFTWARE='docker'
+fi
+
+# $SOFTWARE stop --all && \
+# $SOFTWARE stop $($SOFTWARE ps -aq) && \
+"$SOFTWARE"-compose down && \
+yes | $SOFTWARE system prune && \
+yes | $SOFTWARE volume prune
