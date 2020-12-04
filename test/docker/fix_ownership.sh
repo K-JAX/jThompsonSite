6ee3ed3dcdfea052a52531ceeb225c7787ea0858
Kevin Garubba <kevingarubba@gmail.com>
2020-12-04 11:07:03 -0500

    Cleaning up the Dockerfiles in frontend and wordpress. Removing test directory.

--- test/docker/fix_ownership.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-chown -R www-data:www-data .
-
-sudo -u www-data install_wordpress
--- test/example.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-# Basic if statement
-if [ ! $(wp core verify-checksums) ]
-then
-    # wp core download --force
-    echo 'corret, latest wordpress is not installed in this folder'
-fi
-
