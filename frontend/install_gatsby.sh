4d657dca5de97e48d579e8bf8f724a15b0451c03
Kevin Garubba <kevingarubba@gmail.com>
2020-12-06 17:10:02 -0500

    Fixing the gatsby install check.

--- frontend/install_gatsby.sh
+++ frontend/install_gatsby.sh
@@ -1,25 +1,27 @@
 #!/bin/bash sh
 
-echo "Checking if installed first"
-if npm list | grep gatsby-source-wordpress
+
+
+echo "checking wordpress"
+# wordpress_ready='nc -z wp-headless 8080'
+wordpress_ready='curl -s --head  --request GET localhost:8080'
+
+if ! $wordpress_ready
 then
+    printf 'Waiting for Wordpress.'
+    while ! $wordpress_ready
+    do
+        printf '.'
+        sleep 1
+    done
+fi
+
+echo "Checking if installed first"
+if [ -d node_module ]; then
+    echo "Node modules are installed!"
     gatsby develop -H 0.0.0.0
 else
-
-    echo "checking wordpress"
-    # wordpress_ready='nc -z wp-headless 8080'
-    wordpress_ready='curl -s --head  --request GET localhost:8080'
-
-    if ! $wordpress_ready
-    then
-        printf 'Waiting for Wordpress.'
-        while ! $wordpress_ready
-        do
-            printf '.'
-            sleep 1
-        done
-    fi
-
+    echo "Node modules not installed yet!"
     npm i
     gatsby develop -H 0.0.0.0
 
