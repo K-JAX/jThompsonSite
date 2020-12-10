60cdaf56194343362e3301505c6005542a51c634
Kevin Garubba <kevingarubba@gmail.com>
2020-12-09 21:06:03 -0500

    Removing plugin files from the repo.

--- wordpress/wp-content/plugins/wp-graphql-meta-query-develop/tests/_data/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Disable autoloading while running tests, as the test
- * suite already bootstraps the autoloader and creates
- * fatal errors when the autoloader is loaded twice
- */
-define( 'WPGRAPHQL_METAQUERY_AUTOLOAD', false );
-define( 'CODECEPTION_REMOTE_COVERAGE', true );
